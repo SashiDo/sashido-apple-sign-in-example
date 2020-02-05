7c79b8bed8057c159a62c104a4167f0c89b0691f
Pavel Ivanov <pafelka@gmail.com>
2020-02-05 09:30:14 +0200

    Initial Commit

---
+++ SashiDo-Apple-Sign-In/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SashiDo-Apple-Sign-In
+//
+//  Created by Pavel Ivanov on 5.02.20.
+//  Copyright © 2020 Pavel Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
